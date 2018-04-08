39805b497a2499f5b613cfe703c72e351824f00d
raoasifraza11 <raoasifraza1@hotmail.com>
2018-04-09 00:25:30 +0500

    Added down/unzip

--- ezr-install.sh
+++ ezr-install.sh
@@ -2,6 +2,12 @@
 # Author: Asif Raza #
 # Date: Sun Apr  8 20:09:55 PKT 2018 #
 
+#curl -LJ https://github.com/raoasifraza11/eee-zee-rar/archive/alpha-v1.0.zip -o eee-zee-rar.zip
+
+# TODO: Download should be stored in tmp
+#unzip eee-zee-rar.zip
+
+# do more here
 
 # Write aliases in ~/.bashrc
 # TODO: prevent duplication of content
