083242321be7e8680a6948dd13de40be68d708cc
raoasifraza11 <raoasifraza1@hotmail.com>
2018-04-17 21:46:45 +0500

    Newly logic for install scripts

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Author: Asif Raza #
+# Date: Sun Apr  8 20:09:55 PKT 2018 #
+
+#curl -LJ https://github.com/raoasifraza11/eee-zee-rar/archive/alpha-v1.0.zip -o eee-zee-rar.zip
+
+# TODO: Download should be stored in tmp
+#unzip eee-zee-rar.zip
+
+# do more here
+
