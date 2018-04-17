9c75745cc4d3ac51d9b0da9d828beaa0af75934c
raoasifraza11 <raoasifraza1@hotmail.com>
2018-04-17 23:31:36 +0500

    Load the all config files

---
+++ support/load.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# Author: Asif Raza #
+# Date: Tue Apr 17 22:31:22 PKT 2018 #
+
+init(){
+
+export EZR=$HOME/.eee-zee-rar
+
+}
+
+load_ezr(){
+
+    # Check existence
+    if [ -f ~/.bashrc ]; then
+
+        # Fetch lines
+        while IFS='' read -r line || [[ -n "$line" ]]; do
+        echo "$line" >> ~/.bashrc
+
+        # Escape the lines
+        done < <(tail -n +5 "$EZR/support/script.sh")
+    fi
+
+
+    if [ -f ~/.zshrc ]; then
+
+        # Fetch lines
+        while IFS='' read -r line || [[ -n "$line" ]]; do
+        echo "$line" >> ~/.zshrc
+
+        # Escape the lines
+        done < <(tail -n +5 "$EZR/support/script.sh")
+    fi
+
+    echo -e "EZR setup successfully!! $(date)"
+
+}
+
+truncate_ezr_bashrc(){
+    echo -n '' > ~/.bashrc
+}
+
+
+init
+load_ezr
---
+++ support/script.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Author: Asif Raza #
+# Date: Tue Apr 17 22:33:21 PKT 2018 #
+
+for file in $HOME/.eee-zee-rar/lib/*.sh
+do
+    config $file;
+done
