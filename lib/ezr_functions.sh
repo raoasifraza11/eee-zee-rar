ddf39b326b0adbda81410052dc320bb5d270d201
raoasifraza11 <raoasifraza1@hotmail.com>
2018-04-18 17:46:15 +0500

    Added help funcs

--- lib/ezr_functions.sh
+++ lib/ezr_functions.sh
@@ -6,6 +6,10 @@ ezr(){
     echo "$(tput setaf 2)Hello, From EEE-ZEE-RAR?"
 }
 
+ezr_help(){
+    echo "$(tput setaf 2)Hello, How may I help you?"
+}
+
 ezr_uninstall(){
     bash $HOME/.eee-zee-rar/uninstall.sh
 }
